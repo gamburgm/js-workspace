ed4ff73d1ff9104fde32e14320ade3c3e86b089c
Mitch Gamburg <mitch.gamburg@gmail.com>
2022-02-04 18:21:37 -0500

    ephemeron wip

--- ephemeron.js
+++ ephemeron.js
@@ -1,1 +1,19 @@
-export default 
+export default class Ephemeron {
+  static #primitiveLabel = Symbol('primitive');
+
+  #key;
+
+  #ref;
+
+  // TODO need to add finalization registry
+
+  constructor(key, val) {
+    if (Ephemeron.#canBeWeak(key)) {
+      this.#key = new WeakRef(key);
+    } else {
+      this.#key = new WeakRef(Ephemeron.#makePrimitiveKey(key));
+    }
+
+    this.#ref = 
+  }
+}
