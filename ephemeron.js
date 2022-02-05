30e374ee39db31b366f5c57d0e283d81c921b929
Mitch Gamburg <mitch.gamburg@gmail.com>
2022-02-05 17:04:17 -0500

    correct ephemeron

--- ephemeron.js
+++ ephemeron.js
@@ -2,7 +2,7 @@ export default class Ephemeron {
   static #primitiveLabel = Symbol('primitive');
 
   static #registry = new FinalizationRegistry((ephemeron) => {
-    ephemeron.#clear();
+    ephemeron.#weakenValue();
   });
 
   #key;
@@ -23,13 +23,15 @@ export default class Ephemeron {
     const maybeVal = this.#val;
     if (maybeVal === undefined) {
       return false;
+    } else if (maybeVal instanceof WeakRef) {
+      if (maybeVal.deref() === undefined) {
+        return false;
+      } else {
+        return maybeVal.deref();
+      }
     } else {
       return maybeVal;
     }
-  }
-
-  #clear() {
-    this.val = false;
   }
 
   static #canBeWeak(v) {
@@ -38,5 +40,9 @@ export default class Ephemeron {
 
   static #makePrimitiveKey(key) {
     return { primLabel: Ephemeron.#primitiveLabel, value: key };
+  }
+
+  static #weakenValue() {
+    this.#val = new WeakRef(this.#val);
   }
 }
